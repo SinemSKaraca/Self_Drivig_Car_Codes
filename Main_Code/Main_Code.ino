8d2ecb4d544778d83b2797a37d78a01b6e83e92b
SinemSKaraca <162469642+SinemSKaraca@users.noreply.github.com>
2024-08-03 16:33:37 +0300

    Update Main_Code.ino
    Bazı küçük değişiklikler yapıldı.

--- Main_Code/Main_Code.ino
+++ Main_Code/Main_Code.ino
@@ -7,8 +7,8 @@ const int OUT3 = 7;
 const int OUT4 = 8;
 
 // Motorların hız kontrolü için motor sürücünün pin tanımlamaları:
-const int motor_kontrol_1 = 9; // Motor sürücünün ENA (PWM özellikli) - OUT1/2
-const int motor_kontrol_2 = 10; // Motor sürücünün ENB Pini - OUT3/4
+const int EN_A = 9; // Motor sürücünün ENA (PWM özellikli) - OUT1/2
+const int EN_B = 10; // Motor sürücünün ENB Pini - OUT3/4
 
 // HCSR04 Ultaronik mesafe sensörü pin tanımlamaları:
 const int echoPin = 11;
@@ -26,8 +26,8 @@ decode_results results; // kumandadan gelen sinyali çözmek için türetilen ne
 #define right 16734885 // SAĞ
 #define left 16716015  // SOL
 #define zero 16750695  // ARAÇ DURDURMA
-#define star 16738455  // HIZ ARTTIRMA
-#define kare 16756815  // HIZ AZALTMA
+#define star 16738455  // HIZ AZALTMA
+#define kare 16756815  // HIZ ARTTIRMA
 
 // Bu kısım Arduino'ya ilk kez enerji verildiğinde/kart resetlendiğinde
 // yalnızca bir defa çalışır.
@@ -37,16 +37,16 @@ void setup() {
   pinMode(OUT2, OUTPUT);
   pinMode(OUT3, OUTPUT);
   pinMode(OUT4, OUTPUT);
-  pinMode(motor_kontrol_1, OUTPUT);
-  pinMode(motor_kontrol_2, OUTPUT);
+  pinMode(EN_A, OUTPUT);
+  pinMode(EN_B, OUTPUT);
 
   // HCSR04 sensörünün pinlerinin işlevleri
   pinMode(echoPin, INPUT); // Dışardan gelen sinyali alacak
   pinMode(trigPin, OUTPUT); // Dışarıya ultrasonik sinyal verecek
 
   // Arduino kartına enerji verir vermez aracın hareket etmemesi için:
-  digitalWrite(motor_kontrol_1, LOW);
-  digitalWrite(motor_kontrol_2, LOW);
+  digitalWrite(EN_A, LOW);
+  digitalWrite(EN_B, LOW);
   digitalWrite(OUT1, LOW);
   digitalWrite(OUT2, LOW);
   digitalWrite(OUT3, LOW);
@@ -54,14 +54,64 @@ void setup() {
 
   // IR alıcısının başlatılması:
   ir_alici.enableIRIn();
-
   Serial.begin(9600);
 }
 
 void loop() {
   int mesafe = mesafe_olcumu();
-
-  Serial.println(mesafe);
+  
+  if(mesafe > 40) {
+    // Kumandadan herhangi bir tuşa basılıp basılmadığını kontrol ediyoruz
+    if(ir_alici.decode(&results)) {
+      Serial.println(results.value);
+      switch(results.value) {
+        case kare:
+          if(hiz < 255)
+            hiz += 5;
+          Serial.println("Hizlandi");
+          Serial.println(results.value);
+          break;
+        case star:
+          if(hiz > 80)
+            hiz -= 5;
+          Serial.println("Yavasladi");
+          Serial.println(results.value);
+          break;
+        case up:
+          motor_hareketleri(1, 0, 0, 1, hiz);
+          Serial.println("Ileri gitti");
+          Serial.println(results.value);
+          break;
+        case down:
+          motor_hareketleri(0, 1, 1, 0, hiz);
+          Serial.println("Geri gitti");
+          Serial.println(results.value);
+          break;
+        case left:
+          motor_hareketleri(1, 0, 1, 0, hiz);
+          Serial.println("Sola dondu");
+          Serial.println(results.value);
+          break;
+        case right:
+          motor_hareketleri(0, 1, 0, 1, hiz);
+          Serial.println("Saga dondu");
+          Serial.println(results.value);
+          break;
+        case zero:
+          motor_hareketleri(0, 0, 0, 0, 0);
+          Serial.println("Durdu");
+          Serial.println(results.value);
+          break;
+      }
+      ir_alici.resume();
+    }
+  }
+  else {
+    // Önde engel varsa araç geri gider
+    motor_hareketleri(0, 1, 1, 0, hiz);
+    delay(500);
+    motor_hareketleri(0, 0, 0, 0, 0);
+  }
 }
 
 int mesafe_olcumu() {
@@ -79,6 +129,27 @@ int mesafe_olcumu() {
 
   return mesafe;
 }
+
+// Aracın ileri, geri, sağa, sola hareket etmesini sağlayan fonksiyon
+// İlk 4 parametre hangi motorun hangi yöne döneceğini belirleyecek
+// Parametreler sırasıyla OUT1/2/3/4 ve hız kontrolü için kullanılacak
+void motor_hareketleri(int deger1, int deger2, int deger3, int deger4, int hiz) {
+  // ENA ve ENB'yi(motorların hepsinin aynı hızda çalışmasını istediğimiz için), hiz ile kontrol edeceğiz
+  digitalWrite(OUT1, deger1); // deger1 = 0 ise LOW olur
+  digitalWrite(OUT2, deger2);
+  digitalWrite(OUT3, deger3);
+  digitalWrite(OUT4, deger4);
+
+  analogWrite(EN_A, hiz);
+  analogWrite(EN_B, hiz);
+}
+
+
+
+
+
+
+
 
 
 
